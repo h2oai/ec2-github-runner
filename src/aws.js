9a60c4ccdfa9eb5054a9641a853e90a5cfe8787b
Michal Raška <michalr@h2o.ai>
2022-01-20 22:31:03 +0100

    Update aws.js

--- src/aws.js
+++ src/aws.js
@@ -10,7 +10,6 @@ function buildUserDataScript(githubRegistrationToken, label) {
     return [
       '#!/bin/bash',
       `cd "${config.input.runnerHomeDir}"`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
       `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
       'sudo -u ubuntu ./run.sh',
     ];
@@ -21,7 +20,6 @@ function buildUserDataScript(githubRegistrationToken, label) {
       'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
       'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
       'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
       `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
       'sudo -u ubuntu ./run.sh',
     ];
